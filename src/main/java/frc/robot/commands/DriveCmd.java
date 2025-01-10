9ddd57d007782d999a7277f2d619b8da24d3149f
MrDrGru <3596@gmail.com>
2025-01-09 18:46:51 -0600

    Clean DriveCmd and format

--- src/main/java/frc/robot/commands/DriveCmd.java
+++ src/main/java/frc/robot/commands/DriveCmd.java
@@ -4,8 +4,6 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import frc.robot.subsystems.Constants;
