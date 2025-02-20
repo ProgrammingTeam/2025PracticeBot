130aa12e3f9c1859126d25b82f6dd8e0257e7aae
Loki <loki.sticks@gmail.com>
2025-02-20 17:56:32 -0600

    Worked on the Rotator/Algea manipulator as much as possible.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -157,7 +157,7 @@ public class RobotContainer {
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
+   * Alex was here 2/18/2025
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
