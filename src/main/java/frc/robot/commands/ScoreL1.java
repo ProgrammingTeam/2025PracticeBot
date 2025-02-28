35e4abab254bdbc7bb46d8ce53ee70a71e2eea8c
Loki <loki.sticks@gmail.com>
2025-02-27 18:18:09 -0600

    deleted arm command

---
+++ src/main/java/frc/robot/commands/ScoreL1.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.ElevatorConstants.ElevatorPositions;
+import frc.robot.subsystems.ElevatorSub;
+import frc.robot.subsystems.FunnelSub;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class ScoreL1 extends SequentialCommandGroup {
+
+  /** Creates a new scoreL1. */
+
+  ScoreL1(ElevatorSub m_ElvSub, FunnelSub SubFunnel) {
+
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    // alex was here
+    addCommands(new ParallelRaceGroup (new ElevatorMoveButton(m_ElvSub, ElevatorPositions.L1), new WaitCommand(3)));
+    addCommands(new ParallelRaceGroup (new DispenserCommand(SubFunnel), new WaitCommand(4)));
+    addCommands(new ParallelRaceGroup(new ElevatorMoveButton(m_ElvSub, ElevatorPositions.travel), new WaitCommand(4)));
+  }
+
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    // alex was here
+    // i am sigma
+    // christian yelich looks like a walnut
+    // I mean whoever said that isn't wrong he does kinda look like a walnut but there had to be a nicer way to say that
+}
