b2452412ff76f68239aa54f3981db28a129c0235
MrDrGru <123707817+MrDrGru@users.noreply.github.com>
2025-03-18 09:01:43 -0500

    Formatted and cleaned AlgaeSub.java

--- src/main/java/frc/robot/subsystems/AlgaeSub.java
+++ src/main/java/frc/robot/subsystems/AlgaeSub.java
@@ -10,33 +10,46 @@ import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AlgaeSub extends SubsystemBase {
-  /** Creates a new AlgaeSub./**/
-    public final class Arm{
-     SparkMax armMotor; 
-     
-     public Arm(){
-      armMotor = new SparkMax(0, SparkLowLevel.MotorType.kBrushless);
-     }
-     public void spin(double spin){
-      armMotor.set(spin);
-     }
-     }
-  
-    public final class Rotater{
-      SparkMax rotatorMotor;
 
-      public Rotater(){
-       rotatorMotor = new SparkMax(0, SparkLowLevel.MotorType.kBrushless);
-      }  
-      public void spin(double spin){   
+  // Creates a final nested class "Arm"
+  public final class Arm {
+    SparkMax armMotor; 
+
+    // Arm constructor
+    public Arm() {
+      armMotor = new SparkMax(0, SparkLowLevel.MotorType.kBrushless);
+    }
+
+    // Method to set the speed for the arm motor
+    public void spin(double spin) {
+      armMotor.set(spin);
+      }
+  }
+
+  // Nested class rotator
+  public final class Rotater {
+    SparkMax rotatorMotor;
+
+    // Rotator constuctor
+    public Rotater() {
+      rotatorMotor = new SparkMax(0, SparkLowLevel.MotorType.kBrushless);
+    }
+
+    // Methods to set speed of motor and stop method if needed
+    public void spin(double spin){   
       rotatorMotor.set(spin); 
-      }
-      public void stop(){
-        rotatorMotor.set(0);
-      }
-      }
-    public final Rotater rotater;
-    public final Arm arm;
+    }
+    
+    public void stop() {
+      rotatorMotor.set(0);
+    }
+  }
+
+  // Makes new local, public instances of nested clases
+  public final Rotater rotater;
+  public final Arm arm;
+
+  // Constructor for AlgaeSub -- Parent Class
   public AlgaeSub() {
     rotater = new Rotater();
     arm = new Arm();
@@ -44,8 +57,7 @@ public class AlgaeSub extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-
+  // Runs every scheduler run
   }
 }
 
